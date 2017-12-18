03c91acd000f1d6f23f4a221c09b9c9c5eed3e35
Renato Gama <renatoargh@gmail.com>
2017-12-18 09:57:33 -0200

    Adicionando campos calculados e fixando timeout de 30s para aguardar login e garantir que sempre funciona

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const Big = require('big.js')
+const Moment = require('moment')
 const jcrawler = require('jcrawler')
 const cheerio = require('cheerio')
 const TESOURO_DIRETO_URL = 'https://tesourodireto.bmfbovespa.com.br/portalinvestidor/'
 
 const noop = () => {}
-const parseValor = (valor) => parseFloat(valor.replace(/\./g, '').replace(',', '.'))
-const parseConta = (texto) => {
+const parseData = data => new Moment(data, 'DD/MM/YYYY').toDate()
+const parseValor = valor => parseFloat(valor.replace(/\./g, '').replace(',', '.')) || null
+
+const parseConta = texto => {
   const match = texto.match(/^(.*)\s-\s(.*)\s\(Conta ativa desde\s(.*)\)$/)
 
   if (!match) {
@@ -14,8 +18,26 @@ const parseConta = (texto) => {
   return {
     corretora: match[2],
     conta: match[1],
-    ativaDesde: match[3]
+    ativaDesde: parseData(match[3])
   }
+}
+
+
+function obterValoresCalculados (titulo) {
+  // VALORIZACAO
+  const { valorInvestido, valorLiquidoAtual } = titulo
+  let valorizacao = parseFloat(new Big(valorLiquidoAtual).div(valorInvestido).minus(1).round(6).valueOf())
+  valorizacao = new Big(valorizacao).times(100).round(2).valueOf().replace('.', ',') + '%'
+  if (!valorizacao.startsWith('-')) {
+    valorizacao = '+' + valorizacao
+  }
+
+  // DIAS ATE O VENCIMENTO
+  const hoje = new Moment()
+  const diasAteVencimento = new Moment(titulo.vencimento).diff(hoje, 'days')
+
+  // RESULTADO
+  return { valorizacao, diasAteVencimento }
 }
 
 const tesouroDireto = async (credenciais, cb = noop) => {
@@ -39,50 +61,55 @@ const tesouroDireto = async (credenciais, cb = noop) => {
 
   return await crawler.each(credenciais, async (browser, page, credencial) => {
     // LOGIN
-    await page.goto(TESOURO_DIRETO_URL)
+    await page.goto(TESOURO_DIRETO_URL, { timeout: 60000 })
     await page.waitFor('input#BodyContent_txtLogin')
     await page.type('input#BodyContent_txtLogin', credencial.login)
     await page.type('input#BodyContent_txtSenha', credencial.senha)
     await page.click('input#BodyContent_btnLogar')
 
     // EXTRATO
-    await page.waitForNavigation({ waitUntil: 'load' })
-    await page.goto(TESOURO_DIRETO_URL + '/extrato.aspx', {
-      timeout: 60000
-    })
-
+    // await page.waitForNavigation({ waitUntil: 'load' })
+    await page.waitFor(30000)
+    await page.goto(TESOURO_DIRETO_URL + '/extrato.aspx', { timeout: 60000 })
     await page.click('input#BodyContent_btnConsultar')
-    await page.waitFor('#BodyContent_repSintetico_tblAgenteHeader_0', {
-      timeout: 60000
-    })
+    await page.waitFor('.section-container', { timeout: 60000 })
 
     // PARSING
     const html = await page.content()
     const $ = cheerio.load(html)
+    const titularNome = $('#Topo_lblNome').text().trim()
 
     const resultados = []
-    $('.section-container').each((index, element) => {
-      const $corretora = cheerio.load(element)
+    $('.section-container').each((index, div) => {
+      const $div = cheerio.load(div)
       const resultado = {
-        conta: parseConta($corretora('p.title').text()),
+        titular: {
+          nome: titularNome,
+          cpf: credencial.login
+        },
+        conta: parseConta($div('p.title').text()),
         titulos: []
       }
 
-      $corretora('table tbody tr').each((index, tr) => {
+      $div('table tbody tr').each((index, tr) => {
         const $tr = cheerio.load(tr)
         const dados = $tr('td').map((index, td) => {
           return $(td).text().trim()
         }).get()
 
-        resultado.titulos.push({
+        const titulo = {
           nome: dados[0],
-          vencimento: dados[1],
+          vencimento: parseData(dados[1]),
           valorInvestido: parseValor(dados[2]),
           valorBrutoAtual: parseValor(dados[3]),
           valorLiquidoAtual: parseValor(dados[4]),
           quantidadeTotal: parseValor(dados[5]),
           quantidadeBloqueada: parseValor(dados[6])
-        })
+        }
+
+        titulo.valoresCalculados = obterValoresCalculados(titulo)
+
+        resultado.titulos.push(titulo)
       })
 
       resultados.push(resultado)
@@ -94,7 +121,11 @@ const tesouroDireto = async (credenciais, cb = noop) => {
 
 module.exports = tesouroDireto
 
-// const credenciais = require('./credenciais.js')
-// tesouroDireto(credenciais).then(dados => {
-//   console.log(JSON.stringify(dados, null, 4))
-// })
+const credenciais = require('./credenciais.js')
+// tesouroDireto(credenciais, (err, dados) => {
+tesouroDireto(credenciais).then(dados => {
+  console.log(JSON.stringify(dados, null, 4))
+}).catch(err => {
+  console.log('err')
+  console.log(err)
+})
